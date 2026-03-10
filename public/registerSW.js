e6ab1420ea87546793242d624f904419d14baa6f
meguru-v1 <222714034+meguru-v1@users.noreply.github.com>
2026-03-10 20:41:29 +0900

    fix: radical cache breaking with sw.js neutralizing and caches.clear()

---
+++ public/registerSW.js
@@ -0,0 +1,8 @@
+/* Ghost Register Script */
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.getRegistrations().then(registrations => {
+        for (const registration of registrations) {
+            registration.unregister();
+        }
+    });
+}
